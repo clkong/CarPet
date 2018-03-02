d544eb814d2e0d042b35f8c67c6caaf8a02e5721
toluo7449 <toluo7449@7J2M6Y1.Student.UGDSB.ED>
2018-03-02 13:50:19 -0500

    my second commit

---
+++ src/CarPet/Carpet.java
@@ -0,0 +1,28 @@
+/* Sean Luo
+ * CarPet.java
+ * This progrom calculates the CarPet 
+ */
+
+package CarPet;
+
+/**
+ *
+ * @author toluo7449
+ */
+public class Carpet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       double length=8.5;
+       double width=6;
+       double area=length*width;
+       double cost=area*19.95;     //
+       System.out.println("The cost is= $"+cost);
+               
+            
+    }
+    
+}
+
